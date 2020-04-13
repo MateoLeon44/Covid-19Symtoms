ce2587db2c1205cafec2f7ad56641bf2ea0d3ff4
MateoLeon44 <30735098+MateoLeon44@users.noreply.github.com>
2020-04-13 10:57:26 -0500

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.json());
  */
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "./front/build")));
 
 configurePassport(app);
 app.use('/', indexRouter);
